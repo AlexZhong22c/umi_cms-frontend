59cb438859ff022511fd38c88cd1a1da920c9dc1
ZhongZhaoCong <535377683@qq.com>
2019-12-23 16:05:19 +0800

    feat: 补设置语言locale

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import { ConfigProvider } from 'antd';
+import zh from 'antd/lib/locale-provider/zh_CN';
+
 /**
- * @file 在其他js体系中原本是不用加这一层catch error的。在dva体系中，加这个是为了不让dva错误没有被最终捕获而在下一次js事件中部分代码无法被触发。(原因不明)
+ * 在其他js体系中原本是不用加这一层catch error的。在dva体系中，加这个是为了不让dva错误没有被最终捕获而在下一次js事件中部分代码无法被触发。(原因不明)
  * 只要加了全局onError就没有这种bug了，最终效果和其他js体系一样。
  */
-
 export const dva = {
   config: {
     // 消费1次：该onError能消费一次错误，避免错误没有最终被catch而导致dva体系部分代码无法再被触发。
@@ -14,3 +17,7 @@ export const dva = {
     },
   }
 };
+
+export function rootContainer(container) {
+  return React.createElement(ConfigProvider, { locale: zh }, container);
+}
