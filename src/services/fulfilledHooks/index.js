634625149b102552bfd3d0189cde3db57710490b
ZhongZhaoCong <535377683@qq.com>
2020-02-15 12:17:05 +0800

    style: 修改函数名

--- src/services/fulfilledHooks/index.js
+++ src/services/fulfilledHooks/index.js
@@ -1,20 +1,20 @@
 // request fulfilled 拦截器
-import requestDomainUrl from './requestDomainUrl';
+import requestDecideUrlPrefix from './requestDecideUrlPrefix';
 import requestAddToken from './requestAddToken';
 
 // response fulfilled 拦截器
 import responseServerError from './responseServerError';
 import responseBaseError from './responseBaseError';
-import responseChangeReturn from './responseChangeReturn';
+import responseResetReturn from './responseResetReturn';
 
 // 顺序从该钩子数组 后往前 执行：
 export const requestFulfilledHooks = [
-  requestDomainUrl,
+  requestDecideUrlPrefix,
   requestAddToken
 ];
 // 顺序从该钩子数组 前往后 执行：
 export const responseFulfilledHooks = [
   responseServerError,
   responseBaseError,
-  responseChangeReturn
+  responseResetReturn
 ];
